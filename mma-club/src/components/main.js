dd4e9fb4ca991f2cd47513429c0946e1e17d15a5
Jonathan Yao <jonathmy@usc.edu>
2020-05-22 16:55:27 -0700

    website not working properly

--- mma-club/src/components/main.js
+++ mma-club/src/components/main.js
@@ -7,7 +7,6 @@ import {Switch, Route} from 'react-router-dom';
 const Main = ()=>(
     <Switch>
         <Route exact path="/" component = {Home} />
-        
         <Route path="/about" component = {About} />
         <Route path="/contact" component = {Contact} />
     </Switch>
