b3b48b89cf251bb831012c31438efe4c3503a641
Jonathan Yao <jonathmy@usc.edu>
2020-05-22 16:39:30 -0700

    Updates

---
+++ precache-manifest.f32f8ab5f3ca26b9729c985510f84ef4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2baf76c300905652be4c08bc3c7b4dd8",
+    "url": "/MMA-Club-Website/index.html"
+  },
+  {
+    "revision": "023b8981ed396de450d2",
+    "url": "/MMA-Club-Website/static/css/2.4465fbfd.chunk.css"
+  },
+  {
+    "revision": "a0e17fa42004dca53925",
+    "url": "/MMA-Club-Website/static/css/main.cad3dd93.chunk.css"
+  },
+  {
+    "revision": "023b8981ed396de450d2",
+    "url": "/MMA-Club-Website/static/js/2.c95c2071.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/MMA-Club-Website/static/js/2.c95c2071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e17fa42004dca53925",
+    "url": "/MMA-Club-Website/static/js/main.e3d33509.chunk.js"
+  },
+  {
+    "revision": "5c86b0a735d56c5a2e00",
+    "url": "/MMA-Club-Website/static/js/runtime-main.8501159d.js"
+  },
+  {
+    "revision": "290eb2e62124ae166cbe07f741bc3045",
+    "url": "/MMA-Club-Website/static/media/Aaron.290eb2e6.jpg"
+  },
+  {
+    "revision": "321ab68dfd393c9d6cc3275b9cab1676",
+    "url": "/MMA-Club-Website/static/media/boing.321ab68d.png"
+  },
+  {
+    "revision": "885d8bad8c5ba38c0bc9fbdd4acbde18",
+    "url": "/MMA-Club-Website/static/media/iglogo.885d8bad.jpeg"
+  },
+  {
+    "revision": "acd453b09329a06f77a459b29961deab",
+    "url": "/MMA-Club-Website/static/media/jonny.acd453b0.jpg"
+  }
+]);
