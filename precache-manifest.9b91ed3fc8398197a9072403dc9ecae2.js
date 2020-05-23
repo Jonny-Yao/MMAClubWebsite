7c3effb97566a928133b4b6d44202219369bfcd9
Jonathan Yao <jonathmy@usc.edu>
2020-05-22 17:45:35 -0700

    Updates

---
+++ precache-manifest.9b91ed3fc8398197a9072403dc9ecae2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f666f890f3800895d142d1c6a80e39b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebf4bba6888d88c025de",
+    "url": "/static/css/2.4465fbfd.chunk.css"
+  },
+  {
+    "revision": "ec29b0f1da8b500ef69f",
+    "url": "/static/css/main.cad3dd93.chunk.css"
+  },
+  {
+    "revision": "ebf4bba6888d88c025de",
+    "url": "/static/js/2.bfccb33e.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.bfccb33e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec29b0f1da8b500ef69f",
+    "url": "/static/js/main.20269243.chunk.js"
+  },
+  {
+    "revision": "0ea9fd8db4c9c679c320",
+    "url": "/static/js/runtime-main.1f1241aa.js"
+  },
+  {
+    "revision": "290eb2e62124ae166cbe07f741bc3045",
+    "url": "/static/media/Aaron.290eb2e6.jpg"
+  },
+  {
+    "revision": "321ab68dfd393c9d6cc3275b9cab1676",
+    "url": "/static/media/boing.321ab68d.png"
+  },
+  {
+    "revision": "885d8bad8c5ba38c0bc9fbdd4acbde18",
+    "url": "/static/media/iglogo.885d8bad.jpeg"
+  },
+  {
+    "revision": "acd453b09329a06f77a459b29961deab",
+    "url": "/static/media/jonny.acd453b0.jpg"
+  }
+]);
